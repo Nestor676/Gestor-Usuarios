0bca7b4f682801aec6d7b0f0a344cbaf0da13f4b
Nestor676 <nestorschierse@paucasesnovescifp.cat>
2026-02-13 09:10:36 +0100

    Terminado

---
+++ gestor.js
@@ -0,0 +1,33 @@
+document.getElementById("formulario").addEventListener("submit", function(event){
+    event.preventDefault();
+    
+    const nombre = document.getElementById("nombre").value;
+    const email = document.getElementById("email").value;
+    const lista = document.getElementById("lista");
+
+    if(nombre === "" || email === "" || !email.includes(".")){
+        alert("Ingresa valores correctos");
+    }else {
+        alert("¡¡Nuevo usuario!!");
+    }
+
+    lista.innerHTML += `<li>${nombre} - ${email}</li>`;
+
+    document.getElementById("nombre").value = "";
+    document.getElementById("email").value = "";
+});
+
+function usuariosRNG(){
+    const nombres = ["Daniel", "Marcos", "Jose", "Nestor", "Laura", "Sofia", "Pedro", "Ana"];
+    const emails = ["daniel@gmail.com", "marcos@gmail.com", "jose@gmail.com", "nestor@gmail.com", "laura@gmail.com", "sofia@gmail.com", "pedro@gmail.com", "ana@gmail.com"];
+    const lista = document.getElementById("lista");
+
+    for(let i = 1; i <= 3; i++){
+        let num = Math.floor(Math.random() * 8);
+        
+        console.log(num);
+
+        lista.innerHTML += `<li>${nombres[num]} - ${emails[num]}</li>`;
+    }
+}
+
